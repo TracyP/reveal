ae95ecafacce974cd3007ed30d9ebf1eb33e21a9
Tracy <vancemcindoe@gmail.com>
2025-07-14 20:49:37 +1200

    Update encryptedWords.js

--- encryptedWords.js
+++ encryptedWords.js
@@ -3,12 +3,5 @@ const encryptedWords = [
   "U2FsdGVkX1+YVQkeiAjECxZCGSGcOARRpqH7vOxxUtA=",
   "U2FsdGVkX18GVEtZ9989vBw6YlKMbu2tM72+E5P5dI0=",
   "U2FsdGVkX1/hMeeIXnJhk3AKmYgv+kRfO7zbVNpLwtE=",
-  "U2FsdGVkX1+rjqrop/zPGqpzDzKLgePgYhcCMVfSuHw=",
-  "U2FsdGVkX1+8vhxu2FjXK6grri7654ydJ3tMkLBzeXc=",
-  "U2FsdGVkX18Atb4rwot8DE5Wiva+EoTC4+UIH9n76jc=",
-  "U2FsdGVkX18PnsRmodKy5RCts1crRbcSEczU0HqUS1U=",
-  "U2FsdGVkX1+FNCCDt8rlmQZEERqHkJ9hMZEnetVhSIc=",
-  "U2FsdGVkX1/BVIUt/JG9LtoblcN7ZDKaRiGD/hOgmqo=",
-  "U2FsdGVkX1+LixdtGvryZmRNJfvPZA7IOMXfKU0+hPE=",
   "U2FsdGVkX1/4GW474iGafQxbIAtXaMeR/df56gXFF3o="
 ];
