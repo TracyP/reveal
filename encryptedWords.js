d58dda5c63ba77bb66f0c4184e57fab43633345f
Tracy <vancemcindoe@gmail.com>
2025-07-15 11:21:16 +1200

    Update encryptedWords.js

--- encryptedWords.js
+++ encryptedWords.js
@@ -1,4 +1,4 @@
-const encryptedWords = [
+window.encryptedWords = [
   "U2FsdGVkX1+RsDfwa+YMYbQAbBgDjyzCil0tQ0z6Z1M=",
   "U2FsdGVkX1/wWW8L32pNJTuC3oVy0REXvwepdm79fD8=",
   "U2FsdGVkX18pJ+MACHeHhwxfMkixY/6qasindmP2cLs=",
