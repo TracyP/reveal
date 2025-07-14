ed050914d954343c96987939bd0529a8b669db1f
Tracy <vancemcindoe@gmail.com>
2025-07-14 20:10:25 +1200

    Update encryptedWords.js

--- encryptedWords.js
+++ encryptedWords.js
@@ -1,14 +1,14 @@
 const encryptedWords = [
-  "U2FsdGVkX1+ztdYZfKqXJdQ1yZih/ykGm1VtGFwqNkI=",
-  "U2FsdGVkX18BR7+IjgAvWlUQHKe4/5hQmxgnwUAC9fU=",
-  "U2FsdGVkX1++3E1MfQtV2l+V2AlW6+8p2ik2cIY0yuk=",
-  "U2FsdGVkX19zeyOMq5Q1zDt+92M+3l4cN3qkT8BLD8o=",
-  "U2FsdGVkX1/9CYPcQlIqvZ5G/XZPYsNVkceCrDbE4mM=",
-  "U2FsdGVkX1+4IqPfNzIBeCrnfgqOYAJlbMDKhZL9pwU=",
-  "U2FsdGVkX1+7FSN0xCLMxIGm7AeBb7ZnCO5c8M2wq+4=",
-  "U2FsdGVkX1+M8fFZ+MxXY1mlVvF0XhnclJNkEDZLb9I=",
-  "U2FsdGVkX1+Jh4OpzOJKzhgJydc9oMsk/rnJtkG5R8s=",
-  "U2FsdGVkX1+oln+ViALrKk+ZbLn2BHO1vCns/07bnZ8=",
-  "U2FsdGVkX18g0nv7FZvo70z2sAdhTvk6jlc1m4e2dHk=",
-  "U2FsdGVkX1+e/OUg1xDJ10RGzUMaNTXTLPQYB6Mr2Mo="
+  "U2FsdGVkX1+vTcq/GPjGske+z0qVZzr7zrfhr0YCbOk=",
+  "U2FsdGVkX1+YVQkeiAjECxZCGSGcOARRpqH7vOxxUtA=",
+  "U2FsdGVkX18GVEtZ9989vBw6YlKMbu2tM72+E5P5dI0=",
+  "U2FsdGVkX1/hMeeIXnJhk3AKmYgv+kRfO7zbVNpLwtE=",
+  "U2FsdGVkX1+rjqrop/zPGqpzDzKLgePgYhcCMVfSuHw=",
+  "U2FsdGVkX1+8vhxu2FjXK6grri7654ydJ3tMkLBzeXc=",
+  "U2FsdGVkX18Atb4rwot8DE5Wiva+EoTC4+UIH9n76jc=",
+  "U2FsdGVkX18PnsRmodKy5RCts1crRbcSEczU0HqUS1U=",
+  "U2FsdGVkX1+FNCCDt8rlmQZEERqHkJ9hMZEnetVhSIc=",
+  "U2FsdGVkX1/BVIUt/JG9LtoblcN7ZDKaRiGD/hOgmqo=",
+  "U2FsdGVkX1+LixdtGvryZmRNJfvPZA7IOMXfKU0+hPE=",
+  "U2FsdGVkX1/4GW474iGafQxbIAtXaMeR/df56gXFF3o="
 ];
