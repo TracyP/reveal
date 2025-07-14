24de1d64e7c91e78ef77aeea0b8f4d28e631637a
Tracy <vancemcindoe@gmail.com>
2025-07-14 19:28:35 +1200

    Create encryptedWords.js

---
+++ encryptedWords.js
@@ -0,0 +1,14 @@
+const encryptedWords = [
+  "U2FsdGVkX1+ztdYZfKqXJdQ1yZih/ykGm1VtGFwqNkI=",
+  "U2FsdGVkX18BR7+IjgAvWlUQHKe4/5hQmxgnwUAC9fU=",
+  "U2FsdGVkX1++3E1MfQtV2l+V2AlW6+8p2ik2cIY0yuk=",
+  "U2FsdGVkX19zeyOMq5Q1zDt+92M+3l4cN3qkT8BLD8o=",
+  "U2FsdGVkX1/9CYPcQlIqvZ5G/XZPYsNVkceCrDbE4mM=",
+  "U2FsdGVkX1+4IqPfNzIBeCrnfgqOYAJlbMDKhZL9pwU=",
+  "U2FsdGVkX1+7FSN0xCLMxIGm7AeBb7ZnCO5c8M2wq+4=",
+  "U2FsdGVkX1+M8fFZ+MxXY1mlVvF0XhnclJNkEDZLb9I=",
+  "U2FsdGVkX1+Jh4OpzOJKzhgJydc9oMsk/rnJtkG5R8s=",
+  "U2FsdGVkX1+oln+ViALrKk+ZbLn2BHO1vCns/07bnZ8=",
+  "U2FsdGVkX18g0nv7FZvo70z2sAdhTvk6jlc1m4e2dHk=",
+  "U2FsdGVkX1+e/OUg1xDJ10RGzUMaNTXTLPQYB6Mr2Mo="
+];
